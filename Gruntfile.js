a1bc35c3632aa2977739a6f04a7ad7468e9c8ffb
Alexander Ramos <aramo011@fiu.edu>
2015-12-14 23:19:41 -0500

    Install and run grunt critical task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,20 @@
 module.exports = function(grunt) {
 	// project configuration
 	grunt.initConfig({
-
+		critical: {
+			my_critical: {
+				options: {
+					base: './',
+					css: ['css/style.css',],
+					width: 320,
+					height: 70
+				},
+				src: 'index-test.html',
+				dest: 'index-critical-test.html'
+			}
+		}
 
 	});
+
+	grunt.loadNpmTasks('grunt-critical');
 };
